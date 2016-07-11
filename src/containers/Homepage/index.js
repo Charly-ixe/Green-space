ae76dfdd42a0036033d29401178c255543794df0
Charly <j.gigonnet@gmail.com>
2016-07-11 17:12:36 +0200

    Add files

---
+++ src/containers/Homepage/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+import './styles.scss';
+
+import Emitter from 'core/Emitter';
+
+import {
+  WINDOW_RESIZE
+} from 'config/messages';
+
+import ExampleComponent from 'components/Example';
+
+export default Vue.extend({
+
+  template: require('./template.html'),
+
+  data() {
+
+    return {
+      _hidden: null
+    };
+  },
+
+  created() {
+    this.bind();
+  },
+
+  ready() {
+
+    this.addEventListeners();
+
+  },
+
+  beforeDestroy() {
+
+    this.removeEventListeners();
+  },
+
+  methods: {
+
+    /*
+    * Binding & Events
+    */
+
+    bind() {
+    },
+
+    addEventListeners() {
+      Emitter.on(WINDOW_RESIZE, this.onWindowResize);
+    },
+
+    removeEventListeners() {
+      Emitter.off(WINDOW_RESIZE, this.onWindowResize);
+    },
+
+    onWindowResize({width, height}) {
+      /*eslint-disable */
+      console.log(`Window resize from application with debounce -> width: ${width}px || height: ${height}`);
+      /*eslint-enable */
+    }
+    
+  },
+
+  transitions: {
+
+  },
+
+  components: {
+    'example-component': ExampleComponent
+  }
+});
